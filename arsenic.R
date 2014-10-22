81c9d6adc2863b59bda317fa2bc220e72a251922
John Min <john.c.min@gmail.com>
2014-10-22 18:03:29 -0400

    ran logistic regression and going to start PCA

--- arsenic.R
+++ arsenic.R
@@ -1,5 +1,5 @@
 library(ggplot2)
-
+library(glmnet)
 setwd('/Users/JMin/ML/QuaEra_Insights')
 data <- read.table('arsenic_data.txt')
 names(data)
@@ -20,10 +20,31 @@ plot(table(assoc, switch))
 plot(jitter(arsenic), jitter(switch))
 # overlay histogram plot of arsenic, switch
 
+plot(jitter(arsenic), jitter(dist))
+
 # EDUCATION
 plot(table(educ, switch))
 cor(educ, switch)
 # education is continuous variable, regress educ and educ^2
 
+
 # DISTANCE
 plot(jitter(dist), jitter(switch))
+
+data$arsenic_sq <- data$arsenic^2
+data$dist_sq <- data$dist^2
+data$educ_sq <- data$educ^2
+
+plot(educ, dist)
+plot(jitter(educ), jitter(assoc))
+
+
+# Let us standard/normalize data
+# run PCA on it and extract variance from the data, and maybe include squared variables too
+
+## REGRESSION
+logit <- glm(switch ~ arsenic + arsenic_sq + dist + dist_sq+ assoc + educ + educ_sq, family='binomial', data=data)
+
+logit <- glm(switch ~ arsenic + dist + assoc + educ, family='binomial', data=data)
+summary(logit)
+
